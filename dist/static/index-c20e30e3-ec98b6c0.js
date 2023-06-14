a4158a609b914fa70de971d6c7cb35d8f822c596
0xhaz <28639279+0xhaz@users.noreply.github.com>
2023-06-15 00:35:19 +0800

    fixed tsconfig.json

---
+++ dist/static/index-c20e30e3-ec98b6c0.js
@@ -0,0 +1,1 @@
+import{DocumentPaneProvider as m,DocumentPane as a}from"./index-63f13ccc-66263053.js";import"./sanity-0f989f19.js";import"./index-67732e3e.js";export{m as DocumentPaneProvider,a as default};
